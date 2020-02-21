fcea1254d4bd54daa2097be25ef012950004393a
rezaLUL <rezaulull@gmail.com>
2020-02-21 18:54:27 +0700

    tambah onRestart, onResume, onPause, dan onDestroy

--- AndroidLifecycle/app/src/main/java/id/ac/polinema/androidlifecycle/MainActivity.java
+++ AndroidLifecycle/app/src/main/java/id/ac/polinema/androidlifecycle/MainActivity.java
@@ -3,6 +3,7 @@ package id.ac.polinema.androidlifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,5 +30,27 @@ public class MainActivity extends AppCompatActivity {
 
 
 	// TODO: lengkapi callback untuk onRestart() onResume() onPause() onDestroy()
+	@Override
+	protected void onRestart() {
+		super.onRestart();
+		Toast.makeText(this, "Application On Restart", Toast.LENGTH_SHORT).show();
+	}
 
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Toast.makeText(this, "Application On Resume", Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		Toast.makeText(this, "Application On Pause", Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		Toast.makeText(this, "Application On Destroy", Toast.LENGTH_SHORT).show();
+	}
 }
